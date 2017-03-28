b9653e33b7482796a7395323f86845d3d0c3fde4
hangthadj <scot@steppingstonesmuseum.org>
2017-03-28 13:25:06 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = https://docs.google.com/spreadsheets/d/12o8Toi1aMekLyOk1bEQdc4QBRZ-E6SgoGWVXnJa8-jY/edit#gid=0;
